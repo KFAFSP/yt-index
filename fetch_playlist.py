41a2cb576f7ebca0e0e915d45e9a744b2ea183c0
Karl F. A. Friebel <karl.friebel@friebelnet.de>
2019-07-31 00:27:02 +0200

    Added fetch_playlist.

---
+++ fetch_playlist.py
@@ -0,0 +1,244 @@
+"""Fetch playlist metadata from YouTube."""
+
+from aiohttp import ClientSession
+from lxml import etree
+import re
+
+from util import has_brotli, index_s, is_valid_encoding, parse_int, parse_ts
+
+async def fetch_playlist(playlistId: str, chunk_size: int = 1024):
+    """
+    Fetch all metadata of a YouTube playlist.
+
+    Performs multiple asynchronous HTTP requests to obtain all metadata and
+    playlist items. Does not provide rich YouTube metadata: fields that are
+    included in browse_request queries for modern JS YT-Clients.
+
+    Result:
+    {
+        'id': string,
+        'title': string,
+        'description': string,
+        'thumbnail': string,
+        'length': integer,
+        'views': integer,
+        'uploader': {
+            'name': string,
+            'url': string
+        },
+        'items' [
+            {
+                'id': string,
+                'title': string,
+                'uploader': {
+                    'name': string,
+                    'url': string
+                }
+                'lengthSeconds': integer,
+            }
+        ]
+    }
+    """
+
+    #
+    # Initialize the result
+    #
+
+    playlist = {
+        'id': playlistId,
+        'items': []
+    }
+
+    #
+    # Build the headers for the ClientSession.
+    #
+
+    # Defaults are always accepted.
+    accept_encodings = [
+        'gzip',
+        'deflate'
+    ]
+    # If brotlipy is installed, brotli is also accepted.
+    if has_brotli():
+        accept_encodings.append('br')
+
+    headers = {
+        'Host': 'www.youtube.com',
+        # We do not need to lie about ourselves.
+        'User-Agent': 'Python/3.6 aiohttp/3.5.4',
+        # We cannot deal with anything else.
+        'Accept': 'text/html',
+        'Accept-Encoding': ', '.join(accept_encodings)
+    }
+
+    #
+    # Retrieve all data.
+    #
+
+    # Open a auto-raising ClientSession with default cookie handling.
+    async with ClientSession(headers=headers, raise_for_status=True) as session:
+        # Step 1: Get the initial landing page.
+        async with session.get(
+            'https://www.youtube.com/playlist',
+            params={'list': playlistId}
+        ) as response:
+            # Assert that this really worked the way we wanted.
+            assert response.status == 200
+            assert response.content_type == 'text/html'
+            encoding = response.get_encoding()
+            assert is_valid_encoding(encoding)
+
+            # Retrieve the '//div[@id=""]' node.
+            is_content = lambda x: (x.tag, x.get('id')) == ('div', '')
+            parser = etree.HTMLPullParser(events=('start', 'end'))
+            content, discard = None, True
+            while content is None and not response.content.at_eof():
+                # Feed the parser the next chunk of data.
+                parser.feed((await response.content.read(chunk_size)).decode(encoding))
+                for event, node in parser.read_events():
+                    if event == 'start':
+                        if is_content(node):
+                            # Content node reached, stop discarding.
+                            discard = False
+                        continue
+
+                    if is_content(node):
+                        # Content node finished, exit.
+                        content = node
+                        break
+
+                    if discard:
+                        # Discard everything before this point.
+                        node.clear()
+                        for ancestor in node.xpath('ancestor-or-self::*'):
+                            while ancestor.getprevious() is not None:
+                                del ancestor.getparent()[0]
+
+        #
+        # Parse the playlist header.
+        #
+
+        pl_header = content[0]
+        assert pl_header.get('id') == 'pl-header'
+
+        # Get the thumbnail.
+        pl_thumb = pl_header[0][0]
+        assert pl_thumb.tag == 'img'
+        playlist['thumbnail'] = pl_thumb.get('src')
+
+        # Get the title.
+        pl_title = pl_header[1][0]
+        assert pl_title.tag == 'h1'
+        playlist['title'] = pl_title.text.strip()
+
+        # Get the uploader.
+        pl_uploader = pl_header[1][1][0][0]
+        assert pl_uploader.tag == 'a'
+        playlist['uploader'] = {
+            'name': pl_uploader.text,
+            'url': 'https://www.youtube.com' + pl_uploader.get('href')
+        }
+
+        # Get the length.
+        pl_length = pl_header[1][1][1]
+        assert pl_length.tag == 'li'
+        playlist['length'] = parse_int(pl_length.text, aggressive=True)
+
+        # Get the view count.
+        pl_views = pl_header[1][1][2]
+        assert pl_views.tag == 'li'
+        playlist['views'] = parse_int(pl_views.text, aggressive=True)
+
+        # Get the description.
+        pl_description = pl_header[1][2][0]
+        assert pl_description.tag == 'span'
+        playlist['description'] = pl_description.text.strip()
+
+        #
+        # Parse the playlist items.
+        #
+
+        def parse_item(node):
+            item = {'id': node.get('data-video-id')}
+
+            # Get the video thumbnail.
+            vid_thumb = node[2][0][0][0][0][0][0]
+            assert vid_thumb.tag == 'img'
+            item['thumbnail'] = vid_thumb.get('data-thumb')
+
+            # Get video title.
+            vid_title = node[3][0]
+            assert vid_title.tag == 'a'
+            item['title'] = vid_title.text.strip()
+
+            # Get video uploader.
+            vid_uploader = node[3][1][0]
+            assert vid_uploader.tag == 'a'
+            item['uploader'] = {
+                'name': vid_uploader.text,
+                'url': 'https://www.youtube.com' + vid_uploader.get('href')
+            }
+
+            # Get video length.
+            vid_length = node[6][0][0][0]
+            assert vid_length.tag == 'span'
+            item['lengthSeconds'] = parse_ts(vid_length.text)
+
+            return item
+
+        pl_items = content[1][0][0][0][0]
+        assert pl_items.get('id') == 'pl-load-more-destination'
+
+        playlist['items'] += map(parse_item, pl_items)
+
+        #
+        # Fetch and parse all continuations.
+        #
+
+        load_more = index_s(content, 1, 0, 0, 1)
+        assert load_more.tag == 'button'
+        load_more = load_more.get('data-uix-load-more-href') if load_more is not None else None
+        while load_more is not None:
+            # Request the continuation contents.
+            async with session.get(
+                'https://www.youtube.com' + load_more,
+                headers={'Accept': 'application/json'}
+            ) as response:
+                # Assert that this really worked the way we wanted.
+                assert response.status == 200
+                assert response.content_type == 'application/json'
+                encoding = response.get_encoding()
+                assert is_valid_encoding(encoding)
+
+                # Parse the result data (large!)
+                # TODO: This ought to be streamed as well.
+                data = await response.json()
+                assert 'content_html' in data
+                assert 'load_more_widget_html' in data
+
+                # Parse all new items.
+                parser = etree.HTMLPullParser(events=('end',))
+                parser.feed(data['content_html'])
+                for _, node in parser.read_events():
+                    if node.tag == 'tr':
+                        playlist['items'] += parse_item(node)
+
+                        # Discard everything before this point.
+                        node.clear()
+                        for ancestor in node.xpath('ancestor-or-self::*'):
+                            while ancestor.getprevious() is not None:
+                                del ancestor.getparent()[0]
+
+                # Extract the next continuation link
+                match = re.search(
+                    r'data-uix-load-more-href=\"(.+?)\"',
+                    data['load_more_widget_html']
+                )
+                if match:
+                    # Next continuation link.
+                    load_more = match.group(1)
+                else:
+                    # No more continuations.
+                    load_more = None
+
+    return playlist
---
+++ util.py
@@ -0,0 +1,189 @@
+"""Utility functions."""
+
+from typing import Any, Callable, Iterable, TypeVar
+
+def str_s(x: Any, default: str=None) -> str:
+    """
+    Safely construct a string.
+
+    Instead of 'None' on None, default will be returned.
+    """
+
+    if x is None:
+        return default
+
+    try:
+        return str(x)
+    except:
+        return default
+
+def int_s(x: Any, base: int = 10, default: int=None) -> int:
+    """Safely construct an integer."""
+
+    try:
+        return int(x, base=base)
+    except (TypeError, ValueError):
+        return default
+
+def float_s(x: Any, default: float=None) -> float:
+    """Safely construct a float."""
+
+    try:
+        return float(x)
+    except (TypeError, ValueError):
+        return default
+
+def parse_int(x: str, base: int=10, default: int=None, aggressive: bool=False) -> int:
+    """
+    Attempt to parse an integer value.
+
+    If aggressive is True, all characters will be stripped from x that do not
+    constitute a digit of the specified base. Do not use unless x is known to
+    contain exactly one integer value. Requires a base within [1, 36].
+    """
+
+    import re
+
+    if aggressive:
+        assert 1 <= base <= 36
+        digits = '0-{0}{1}'.format(
+            chr(ord('0') + min(base, 10) - 1),
+            ('a-' + chr(ord('a') + base - 11)) if base > 10 else ''
+        )
+        x = re.sub('[^{0}]'.format(digits), '', x)
+
+    return int_s(x, base=base, default=default)
+
+def parse_ts(x: str, default=None) -> int:
+    """
+    Attempt to parse a simple timestamp.
+
+    Timestamp may either be in h+:mm:ss or mm:ss format.
+
+    Result is always in seconds.
+    """
+
+    import re
+
+    match = re.match(r'(?:(\d+):)?(\d{2}):(\d{2})', x)
+    if not match:
+        return default
+
+    (h, m, s) = tuple(int_s(x, default=0) for x in match.groups())
+    if m > 60 or s > 60:
+        return default
+
+    return s + m*60 + h*3600
+
+def index_s(x: Any, *indices, default=None) -> Any:
+    """
+    Attempt to obtain a value by indexing multiple times.
+
+    If the indexing fails because of a TypeError, IndexError or KeyError, the
+    default will be returned.
+    """
+
+    try:
+        ret = x
+        for index in indices:
+            ret = ret[index]
+        return ret
+    except (TypeError, IndexError, KeyError):
+        return default
+
+def join_s(c: str, it: Iterable[str], default: str=None) -> str:
+    """
+    Safely join strings.
+
+    If an item in the iterable is None, it will be replaced with the default
+    before joining.
+
+    All items that are none on joining will be omitted.
+    """
+
+    if default is None:
+        return c.join(filter(lambda x: x is not None, it))
+    return c.join(map(lambda x: x or default, it))
+
+def run_sync(x, *args, **kwargs):
+    """
+    Execute a function synchronously.
+
+    If x is an awaitable or a future, x is awaited and the result is returned.
+
+    If x is a coroutine function, x is invoked with (*args, **kwargs), and the
+    resulting coroutine is awaited and the result is returned.
+
+    If x is some other function, x is invoked with (*args, **kwargs), and the
+    result is returned.
+    """
+
+    import asyncio
+
+    if asyncio.iscoroutine(x):
+        pass
+    elif asyncio.iscoroutinefunction(x):
+        x = x(*args, **kwargs)
+    elif callable(x):
+        return x(*args, **kwargs)
+
+    x = asyncio.ensure_future(x)
+    loop = asyncio.get_event_loop()
+    ret = loop.run_until_complete(x)
+    return ret
+
+T, K = TypeVar('T'), TypeVar('K')
+
+def unique(iterable: Iterable[T], get_key: Callable[[T], K] = lambda x: x) -> Iterable[T]:
+    """
+    Iterate over all unique values in the input sequence.
+
+    Every element with a unique key will be yielded exactly once, and the
+    order of these elements will be preserved.
+
+    get_key will be used to obtain the key for every object in the iterable.
+    """
+
+    seen = set()
+    for x in iterable:
+        if get_key(x) in seen:
+            continue
+        seen.add(x)
+        yield x
+
+def coalesced(iterable: Iterable[T], eq: Callable[[T, T], bool] = lambda x,y: x == y) -> Iterable[T]:
+    """
+    Iterate over the coalesced sequence.
+
+    Only the first element in any consecutive run of elements of the same
+    equivalency class will be yielded, all following items in the run will be
+    discarded.
+    """
+
+    it = iter(iterable)
+    last = next(it)
+    yield last
+    while True:
+        cur = next(it)
+        if eq(cur, last):
+            continue
+        last = cur
+        yield cur
+
+def has_brotli():
+    """Check whether brotli compression is available."""
+
+    try:
+        import brotli
+        return True
+    except ModuleNotFoundError:
+        return False
+
+def is_valid_encoding(encoding: str) -> bool:
+    """Check whether the specified encoding exists."""
+
+    try:
+        ''.encode(encoding)
+        return True
+    except LookupError:
+        return False
